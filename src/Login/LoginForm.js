8c1d4232315571193b909c2591b39de382bd2dc3
aurimasdarguzis <aurimasdarguzis@gmail.com>
2020-09-19 20:50:05 +0100

    feat: add custom datepicker

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -3,7 +3,9 @@ import { useForm } from "react-hook-form";
 
 export default function LoginForm({ history }) {
   const { register, handleSubmit, errors } = useForm();
-  const dateRegex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/i;
+  const calendarDayRegex = /^([1-9]|[12][0-9]|3[01])$/;
+  const calendarMonthRegex = /^[1-9]$|^[1][0-2]$/;
+  const calendarYearRegex = /(19|20)\d\d/;
   const onSubmit = () => {
     history.push("/step-2");
   };
@@ -20,52 +22,97 @@ export default function LoginForm({ history }) {
         aria-required='true'
         aria-labelledby='lastName'
         aria-invalid={errors.lastName ? "true" : "false"} // screen reader will say: "Lastname, edit, invalid entry. This is required"
-        ref={register({ required: true, maxLength: 50 })}
-      />
-      {errors.lastName?.type === "required" && (
-        <span aria-labelledby='lastName' role='alert' className='error-message'>
-          Last name is required
-        </span>
-      )}
-      {errors.lastName?.type === "maxLength" && (
-        <span aria-labelledby='lastName' role='alert' className='error-message'>
-          Maximum limit of characters exceeded
-        </span>
-      )}
-      <label htmlFor='dateOfBirth'>Date of birth (DD/MM/YYYY)</label>
-      <input
-        type='text'
-        name='dateOfBirth'
-        aria-required='true'
-        aria-labelledby='dateOfBirth'
-        aria-invalid={errors.dateOfBirth ? "true" : "false"}
         ref={register({
-          required: true,
-          pattern: {
-            value: dateRegex,
+          required: "Last name is required!",
+          maxLength: {
+            value: 50,
+            message: "Maximum limit of characters exceeded",
           },
         })}
       />
-      {errors.dateOfBirth?.type === "required" && (
-        <span
-          aria-labelledby='dateOfBirth'
-          role='alert'
-          className='error-message'
-        >
-          Date of birth is required
+      {errors.lastName?.message && (
+        <span aria-labelledby='lastName' role='alert' className='error-message'>
+          {errors.lastName.message}
         </span>
       )}
-      {errors.dateOfBirth?.type === "pattern" && (
-        <span
-          aria-labelledby='dateOfBirth'
-          role='alert'
-          className='error-message'
-        >
-          Please enter correct date
-        </span>
-      )}
-      <label htmlFor='postcode'>Postcode</label>
 
+      <label htmlFor='dateOfBirth'>Date of birth</label>
+      {/** Good candidate to extract into component  */}
+      <div className='date-widget'>
+        <div className='user-input-block'>
+          <label htmlFor='day'>Day</label>
+          <input
+            type='text'
+            name='day'
+            aria-required='true'
+            aria-labelledby='day'
+            aria-invalid={errors.day ? "true" : "false"}
+            ref={register({
+              required: "A day is requried",
+              pattern: {
+                value: calendarDayRegex,
+                message: "A day needs to be between 1 and 31",
+              },
+            })}
+          />
+        </div>
+
+        <div className='user-input-block'>
+          <label htmlFor='month'>Month</label>
+          <input
+            type='text'
+            name='month'
+            aria-required='true'
+            aria-labelledby='month'
+            aria-invalid={errors.day ? "true" : "false"}
+            ref={register({
+              required: "A month is requried",
+              pattern: {
+                value: calendarMonthRegex,
+                message: "A month needs to be a number between 1 and 12",
+              },
+            })}
+          />
+        </div>
+        <div className='user-input-block'>
+          <label htmlFor='year'>Year</label>
+          <input
+            type='text'
+            name='year'
+            aria-required='true'
+            aria-labelledby='year'
+            aria-invalid={errors.day ? "true" : "false"}
+            ref={register({
+              required: "A year is requried",
+              maxLength: {
+                value: 4,
+                message: "Please enter valid year",
+              },
+              pattern: {
+                value: calendarYearRegex,
+                message: "Please enter valid year",
+              },
+            })}
+          />
+        </div>
+      </div>
+      {errors.day?.message && (
+        <span aria-labelledby='day' role='alert' className='error-message'>
+          {errors.day.message}
+        </span>
+      )}
+      {errors.month?.message && (
+        <span aria-labelledby='month' role='alert' className='error-message'>
+          {errors.month.message}
+        </span>
+      )}
+      {errors.year?.message && (
+        <span aria-labelledby='year' role='alert' className='error-message'>
+          {errors.year.message}
+        </span>
+      )}
+
+      <label htmlFor='postcode'>Postcode</label>
       <input
         name='postcode'
         type='text'
@@ -73,21 +120,15 @@ export default function LoginForm({ history }) {
         aria-required='true'
         aria-labelledby='postcode'
         aria-invalid={errors.lastName ? "true" : "false"}
-        ref={register({ required: true, minLength: 5, maxLength: 7 })}
+        ref={register({
+          required: "Post code is required",
+          minLength: { value: 5, message: "Minimum 5 characters" },
+          maxLength: { value: 7, message: "Maximum character length exceeded" },
+        })}
       />
-      {errors.postcode?.type === "required" && (
+      {errors.postcode?.message && (
         <span aria-labelledby='postcode' role='alert' className='error-message'>
-          Post code is required
-        </span>
-      )}
-      {errors.postcode?.type === "minLength" && (
-        <span aria-labelledby='postcode' role='alert' className='error-message'>
-          Minimum 5 characters
-        </span>
-      )}
-      {errors.postcode?.type === "maxLength" && (
-        <span aria-labelledby='postcode' role='alert' className='error-message'>
-          Maximum character length exceeded
+          {errors.postcode.message}
         </span>
       )}
       <button aria-label='login' type='submit'>
